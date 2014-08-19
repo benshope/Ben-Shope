eeda0b909a6f33bb7ed1027df44c71e06b46e9f7
benshope <nimajnebs@gmail.com>
2014-08-19 01:13:44 -0400

    Giant Update

--- static/js/script.js
+++ static/js/script.js
@@ -1,33 +1,19 @@
-
-
 // SCROLLING
 $(function() {
   $('#topnav a, .site-title').on('click', function(e) {
     e.preventDefault();
     var scrolldiv = $(this).attr('href');
-    $(scrolldiv).animatescroll({ padding:50 });
+    $(scrolldiv).animatescroll({ padding:60 });
   });
 });
 
-$(window).scroll(function() {
-if ($(".navbar").offset().top > 30) {
-    $(".navbar-fixed-top").addClass("sticky");
-}
-else {
-    $(".navbar-fixed-top").removeClass("sticky");
-  }
-});
-
-
-// PLUGINS
+// ON PAGE LOAD
 var map;
 $(document).ready(function() {
-  
-  // activate the second carousel
-  $('#slider-carousel').carousel({ interval: false });
+  // BOOTSTRAP QUOTE CAROUSEL
   $('#testimonials-carousel').carousel({ interval: false });
   
-  // init the google map plugin
+  // MAP
   map = new GMaps({
     el: '#map',
     lat: 37.3894,
@@ -65,8 +51,7 @@ $(document).ready(function() {
     }]
   });
 
-  
-  // ajax contact form
+  // CONTACT FORM
   $('#contact-form').submit(function(){
       $('#contact-form button').html('Sending...')
       $.post('mail', $(this).serialize(), function(data) {
@@ -77,12 +62,11 @@ $(document).ready(function() {
   });
 });
 
-
+// ANALYTICS
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
 ga('create', 'UA-17852816-3', 'benshope.com');
 ga('send', 'pageview');
 
